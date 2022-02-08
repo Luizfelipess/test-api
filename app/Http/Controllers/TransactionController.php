474b70183e75f173dd69a163ec59d83904183e23
Luiz Silva <lipe0895@hotmail.com>
2022-02-08 12:54:02 -0300

    Adding email with key for creating new transactions

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\UserNotFoundException;
 use App\Http\Requests\Transaction\CreateTransactionRequest;
 use App\Http\Resources\TransactionCollection;
 use App\Interfaces\TransactionRepositoryInterface;
+use App\Interfaces\UserRepositoryInterface;
+use App\Models\Transaction;
+use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
 /**
  * Transaction Controller
  */
@@ -16,21 +22,26 @@ class TransactionController extends Controller
      * @param TransactionRepositoryInterface $transactionRepository
      * @param CreateTransactionRequest $request
      * @return TransactionCollection
+     * @throws Exception
+     * @throws UserNotFoundException
      */
     public function create(
         TransactionRepositoryInterface $transactionRepository,
-        CreateTransactionRequest $request
+        CreateTransactionRequest $request,
+        UserRepositoryInterface $userRepository,
+        Transaction $transactionModel
     ): TransactionCollection {
 
     $data = $request->validated();
 
     $payload = [
         'value' => $data['value'],
-        'payee_id' => $data['payee'],
-        'payer_id' => $data['payer']
+        'payer_id' => $transactionRepository->findByEmail($data['payer'])->id,
+        'payee_id' => $transactionRepository->findByEmail($data['payee'])->id
     ];
-
+        
     $user = $transactionRepository->create($payload);
     return new TransactionCollection($user);
+    
     }
 }
